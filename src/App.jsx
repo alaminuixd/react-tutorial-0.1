c6bc71661d0749b16684f48ec7f2f91a3ec77595
alamin khan <alaminuixd@gmail.com>
2024-09-14 21:57:36 +0600

    changed file path in the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactIcon from "./components/09-react-icons/ReactIcon";
+import ReactIcon from "./components/08-react-icons/ReactIcon";
 
 function App() {
   return (
