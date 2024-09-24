6971b9c79ff0ee4fd04928dbbf6f4acb3e9e986e
alaminuixd <alaminuixd@gmail.com>
2024-09-24 23:16:32 +0600

    14-form added new file Form6.jsx 2

--- src/components/14-form/Form6.jsx
+++ src/components/14-form/Form6.jsx
@@ -62,3 +62,13 @@ function Form6() {
 }
 
 export default Form6;
+/* 
+1) In useState, there are two array elements: val (the current state) and setVal (the function to update the state).
+2) Initially, val contains an object with default properties: { fName: "", lName: "", email: "", password: "" }.
+3) The handleFields function is responsible for updating specific fields in the val object when input changes: 
+  a) It calls setVal with a callback function that returns a new object. 
+  b) The callback function takes prev, which represents the previous state. 
+  c) The ...prev spreads the previous state (keeping all current values), and then [e.target.name] dynamically updates the field based on the input’s name attribute, with e.target.value as its new value. 
+  d) When another field is updated, the previous values remain unchanged, and only the updated field is modified. e) All these changes are stored in val, reflecting the most recent form state.
+4) When the form is submitted, handleSubmit prevents the default behavior and logs the val object, which contains the latest values of all fields.
+*/
